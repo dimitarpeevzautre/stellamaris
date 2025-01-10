becc6af2007b8f399fb126a8ddb37d265260be0f
dimitar.peev <dimitar.peev@gmail.com>
2025-01-10 12:50:21 +0000

    unfinished fade in

---
+++ assets/global.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+  var heroImage = document.querySelector('.hero-image');
+  var scrollPosition = window.pageYOffset;
+
+  // Adjust the factor to control the parallax effect
+  var backgroundPositionY = (scrollPosition * 0.2 - 100) + 'px'; 
+  heroImage.style.backgroundPositionY = backgroundPositionY;
+});
+
--- assets/paralax.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var heroImage = document.querySelector('.hero-image');
-    var scrollPosition = window.pageYOffset;
-  
-    // Adjust the factor to control the parallax effect
-    var backgroundPositionY = (scrollPosition * 0.2 - 100) + 'px'; 
-    heroImage.style.backgroundPositionY = backgroundPositionY;
-  });
